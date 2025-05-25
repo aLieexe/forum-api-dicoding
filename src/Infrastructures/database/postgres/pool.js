a5fd6903220643a4007cbae7ad03525e333d1d31
alieexe <rizky.alay@gmail.com>
2025-05-25 16:42:53 +0700

    yes

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -9,13 +9,5 @@ const testConfig = {
   database: process.env.PGDATABASE_TEST || process.env.PGDATABASE,
 };
 
-console.log('D-E-B-U-G  C-O-N-N-E-C-T-I-O-N:');
-console.log('H-O-S-T:', process.env.PGHOST ? process.env.PGHOST.split('').join('-') : 'localhost');
-console.log('P-O-R-T:', process.env.PGPORT || '5432');
-console.log('U-S-E-R:', process.env.PGUSER ? process.env.PGUSER.split('').join('-') : 'developer');
-// don't log password directly
-console.log('P-A-S-S: [REDACTED]');
-console.log('D-B:', process.env.PGDATABASE ? process.env.PGDATABASE.split('').join('-') : 'forumapi_test');
-
 const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
 module.exports = pool;
