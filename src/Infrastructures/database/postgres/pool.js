45f9e7fb953a2518d57cd6a6e21864f2614bddd8
alieexe <rizky.alay@gmail.com>
2025-05-25 16:12:23 +0700

    ci fixed

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -2,11 +2,11 @@
 const { Pool } = require('pg');
 
 const testConfig = {
-  host: process.env.PGHOST_TEST,
-  port: parseInt(process.env.PGPORT_TEST, 10),
-  user: process.env.PGUSER_TEST,
-  password: process.env.PGPASSWORD_TEST,
-  database: process.env.PGDATABASE_TEST,
+  host: process.env.PGHOST_TEST || process.env.PGHOST,
+  port: parseInt(process.env.PGPORT_TEST || process.env.PGPORT, 10),
+  user: process.env.PGUSER_TEST || process.env.PGUSER,
+  password: process.env.PGPASSWORD_TEST || process.env.PGPASSWORD,
+  database: process.env.PGDATABASE_TEST || process.env.PGDATABASE,
 };
 
 const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
